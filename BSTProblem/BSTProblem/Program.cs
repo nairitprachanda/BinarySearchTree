69927ab11537d86cc6d0a1a370299bffa89ef550
nairitprachanda <nairitprachanda@gmail.com>
2020-11-28 15:32:45 +0530

    [nairit] refactor: creating BST

---
+++ BSTProblem/BSTProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+namespace BSTProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Binary Search Tree Problem!");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            Console.WriteLine("Binary Search Tree: ");
+            binarySearchTree.Display();
+        }
+    }
+}
